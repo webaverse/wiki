221c2e1a0caeee525dc22a0f1049f9df7c2f24b9
Linn Bedroc <blindcrone@tuta.io>
2022-10-03 06:23:29 -0700

    Perhaps better prompting

--- generators/image/character.js
+++ generators/image/character.js
@@ -1,11 +1,12 @@
 import {generateImage} from './generate-image.js';
 import {generateImageMass} from './generate-image.js';
+const characterSettings = { 
+    modelName: 'webaverse_characters',
+    prefix: 'head to toe view of ',
+    suffix: 'anime character concept art, fashion design, detailed face, trending on ArtStation',
+};
 
-export const generateCharacterImage = generateImage({
-  modelName: 'webaverse_characters',
-  prefix: ``,
-  suffix: `anime character concept art, single character, highly detailed, trending on ArtStation`,
-});
+export const generateCharacterImage = generateImage(characterSettings);
 /*
 export const generateCharacterGallery = generateImageMass({
   modelName: 'webaverse_characters',
