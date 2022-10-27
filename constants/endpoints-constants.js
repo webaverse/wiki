06224334a7b1388689230ac626e8adf49bb657b1
listofbanned <listofbanned@gmail.com>
2022-10-27 18:48:51 -0300

    Fix typos

--- constants/endpoints-constants.js
+++ constants/endpoints-constants.js
@@ -3,12 +3,12 @@ export const gpt3Url = `https://gpt3.webaverse.com`; // create
 export const voiceUrl = `https://voice-cw.webaverse.com`;
 export const diffsoundUrl = `https://diffsound.webaverse.com`;
 export const motionDiffusionUrl = `https://motion-diffusion.webaverse.com`; // create
-export const stableDreamfusionUrl = `https://stable-dreamfussion.webaverse.com`; // create
+export const stableDreamfusionUrl = `https://stable-dreamfusion.webaverse.com`; // create
 export const get3dUrl = `https://get-3d.webaverse.com`; // create
 export const musicGeneratorUrl = `https://music-generator.webaverse.com`; // create
-export const discoDiffusionUrl = `https://disco-diffusion.weabaverse.com`; // create
+export const discoDiffusionUrl = `https://disco-diffusion.webaverse.com`; // create
 // coreweave
-export const pixelArtUrl = `https://pixel-art.weabaverse.com`;
-export const weaviateUrl = `https://weaviate.weabaverse.com`;
+export const pixelArtUrl = `https://pixel-art.webaverse.com`;
+export const weaviateUrl = `https://weaviate.webaverse.com`;
 export const stableDiffusionUrl = `https://stable-diffusion.webaverse.com`;
 export const blipUrl = `https://blip.webaverse.com`;
