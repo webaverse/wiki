be5fab947c818e3522d4d90436d6fa54c3a70c1e
listofbanned <listofbanned@gmail.com>
2022-10-27 16:23:33 -0300

    Add coreweave endpoints

--- constants/endpoints-constants.js
+++ constants/endpoints-constants.js
@@ -1,12 +1,14 @@
 export const appUrl = `https://local.webaverse.com`;
-export const gpt3Url = `https://gpt3.webaverse.com`; // check | create
+export const gpt3Url = `https://gpt3.webaverse.com`; // create
 export const voiceUrl = `https://voice-cw.webaverse.com`;
-// export const voiceUrl = `http://207.53.234.253` // coreweave
-export const stableDiffusionUrl = `https://stable-diffusion.webaverse.com`;
 export const diffsoundUrl = `https://diffsound.webaverse.com`;
-export const motionDiffusionUrl = `https://motion-diffusion.webaverse.com`; // check | create
-export const stableDreamfusionUrl = `https://stable-dreamfussion.webaverse.com`; // check | create
-export const get3dUrl = `https://get-3d.webaverse.com`; // check | create
-export const musicGeneratorUrl = `https://music-generator.webaverse.com`; // check | create
-export const weaviateUrl = `https://weaviate.weabaverse.com`; // test
-export const discoDiffusionUrl = `https://disco-diffusion.weabaverse.com`; // check | create
+export const motionDiffusionUrl = `https://motion-diffusion.webaverse.com`; // create
+export const stableDreamfusionUrl = `https://stable-dreamfussion.webaverse.com`; // create
+export const get3dUrl = `https://get-3d.webaverse.com`; // create
+export const musicGeneratorUrl = `https://music-generator.webaverse.com`; // create
+export const discoDiffusionUrl = `https://disco-diffusion.weabaverse.com`; // create
+// coreweave
+export const pixelArtUrl = `https://pixel-art.weabaverse.com`;
+export const weaviateUrl = `https://weaviate.weabaverse.com`;
+export const stableDiffusionUrl = `https://stable-diffusion.webaverse.com`;
+export const blipUrl = `https://blip.webaverse.com`;
