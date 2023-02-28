7f07f51d11776c565d5358072188f4f149f39ef6
Avaer Kazmer <a@modules.io>
2023-02-28 16:27:00 -0500

    Update context

--- clients/context.js
+++ clients/context.js
@@ -1,12 +1,26 @@
 import {DatabaseClient} from './database/database-client.js';
 import {StorageClient} from './storage/storage-client.js';
 import {AiClient} from './ai/ai-client.js';
+import {
+  OPENAI_API_KEY,
+  OPENAI_ACCESS_TOKEN,
+} from '../src/constants/auth.js';
+
+//
 
 export class Ctx {
   constructor() {
-    this.databaseClient = new DatabaseClient();
+    const apiKey = OPENAI_API_KEY;
+    const accessToken = OPENAI_ACCESS_TOKEN;
+    this.aiClient = new AiClient({
+      apiKey,
+      accessToken,
+    });
+
+    this.databaseClient = new DatabaseClient({
+      aiClient: this.aiClient, // for embeddings
+    });
     this.storageClient = new StorageClient();
-    this.aiClient = new AiClient();
   }
 }
 
